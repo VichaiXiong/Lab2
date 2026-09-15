df9a0f3014f18a1b25bfeddcc73a8f3d1a7a11e3
VichaiXiong <79602251+VichaiXiong@users.noreply.github.com>
2026-09-15 00:16:50 -0500

    Add files via upload

---
+++ Vichai Xiong - Lab 2 - Flower.py
@@ -0,0 +1,186 @@
+import turtle
+
+def drawSquare(myTurtle, sideLength):
+    myTurtle.forward(sideLength)
+    myTurtle.right(90)
+    
+    myTurtle.forward(sideLength)
+    myTurtle.right(90)
+   
+    myTurtle.forward(sideLength)
+    myTurtle.right(90)
+   
+    myTurtle.forward(sideLength)
+    myTurtle.right(60)
+
+# def drawSquare2(myTurtle, sideLength):
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(90)
+    
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(90)
+   
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(90)
+   
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(60)
+# def drawSquare3(myTurtle, sideLength):
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(90)
+    
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(90)
+   
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(90)
+   
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(60)
+# def drawSquare4(myTurtle, sideLength):
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(90)
+    
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(90)
+   
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(90)
+   
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(60)
+# def drawSquare5(myTurtle, sideLength):
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(90)
+    
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(90)
+   
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(90)
+   
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(60)
+# def drawSquare6(myTurtle, sideLength):
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(90)
+    
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(90)
+   
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(90)
+   
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(60)
+# def drawSquare7(myTurtle, sideLength):
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(90)
+    
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(90)
+   
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(90)
+   
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(60)
+# def drawSquare8(myTurtle, sideLength):
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(90)
+    
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(90)
+   
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(90)
+   
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(60)
+    
+# def drawSquare9(myTurtle, sideLength):
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(90)
+    
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(90)
+   
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(90)
+   
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(60)
+# def drawSquare10(myTurtle, sideLength):
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(90)
+    
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(90)
+   
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(90)
+   
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(60)
+# def drawSquare11(myTurtle, sideLength):
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(90)
+    
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(90)
+   
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(90)
+   
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(60)
+# def drawSquare12(myTurtle, sideLength):
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(90)
+    
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(90)
+   
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(90)
+   
+#     myTurtle.forward(sideLength)
+#     myTurtle.right(60)
+
+
+def drawFlower(numSquares):
+    for i in range(numSquares):
+        drawSquare(t, 100)
+        t.right(60)
+
+   
+ 
+    
+    
+    
+        
+    
+t = turtle.Turtle()
+
+
+
+#drawSquare(t, 100)
+#drawSquare2(t, 100)
+#drawSquare3(t, 100)
+#drawSquare4(t, 100)
+#drawSquare5(t, 100)
+#drawSquare6(t, 100)
+#drawSquare7(t, 100)
+#drawSquare8(t, 100)
+#drawSquare9(t, 100)
+#drawSquare10(t, 100)
+#drawSquare11(t, 100)
+#drawSquare12(t, 100)
+drawFlower(13)
+
+x = ""
+input(x)
+print(x)
+
+
+    
