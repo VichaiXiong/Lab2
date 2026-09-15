c19742793594e72d48fa4aae49dc949e8d05f81f
VichaiXiong <79602251+VichaiXiong@users.noreply.github.com>
2026-09-15 00:58:32 -0500

    Add files via upload

---
+++ Vichai Xiong - Lab 2 - Star.py
@@ -0,0 +1,31 @@
+import turtle
+
+def drawStar(myTurtle, sideLength):
+    for i in range(5):
+        myTurtle.forward(sideLength)
+        myTurtle.right(144)
+        myTurtle.forward(sideLength)
+        myTurtle.left(72)
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+t = turtle.Turtle()     
+        
+drawStar(t, 200)     
+        
+x= ""  
+input(x)
+print(x)
